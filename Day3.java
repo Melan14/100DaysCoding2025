72cf2cbd8888d81005e7ed303787bb093b8c14a8
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-09-10 02:07:10 +0800

    Memahami dan menggunakan Komentar

---
+++ Day3.java
@@ -0,0 +1,23 @@
+public class Day3 {
+        public static void main(String[] args) {
+            // Komentar satu baris: mencetak teks ke layar
+            System.out.println("Halo, ini contoh komentar di Java!");
+
+        /*
+           Komentar multi-baris:
+           Baris berikut akan menampilkan hasil penjumlahan
+           dari dua bilangan
+        */
+            int a = 5;
+            int b = 3;
+            int hasil = a + b;
+            System.out.println("Hasil penjumlahan: " + hasil);
+
+            /**
+             * Komentar Javadoc:
+             * Bagian ini biasanya dipakai untuk dokumentasi program.
+             * Misalnya menjelaskan fungsi, parameter, atau output.
+             **/
+            System.out.println("Program selesai dijalankan.");
+    }
+}
