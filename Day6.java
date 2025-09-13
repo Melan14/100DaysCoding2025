cfce6ae60c0bab77b693297472c90cbf5d61fb25
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-09-13 20:16:00 +0800

    Tipe Data Pecahan (double, float)

---
+++ Day6.java
@@ -0,0 +1,8 @@
+public class Day6 {
+    public static void main(String[] args) {
+        float f = 18.345f; // float memiliki presisi 7 digit desimal
+        double d = 3.837493849349; //double memiliki presisi 15–16 digit desimal
+        System.out.println("angka float : "+f);
+        System.out.print("angka double : "+d);
+    }
+}
