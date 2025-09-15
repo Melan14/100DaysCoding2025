5bcac0d74b8ced2e0a05cd6e8a55107d463b6b34
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-09-16 00:51:14 +0800

    Deklarasi, Inisialisasi dan Update Variabel

---
+++ Day9.java
@@ -0,0 +1,20 @@
+public class Day9 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int umur;
+        String nama;
+
+        // Inisialisasi
+        umur = 18;
+        nama = "Melan";
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+
+        // Update
+        umur = 20;
+        nama = "Melani Raita";
+        System.out.println("Nama setelah update: " + nama);
+        System.out.println("Umur setelah update: " + umur);
+    }
+}
+
