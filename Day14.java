abb32b215b086874d199492bc162ba277a825d21
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-09-21 03:15:11 +0800

    Mengubah String menjadi Tipe Data Primitif

---
+++ Day14.java
@@ -0,0 +1,12 @@
+public class Day14 {
+    public static void main(String[] args) {
+        String a = "100";
+        String b = "12.5";
+
+        int x = Integer.parseInt(a);
+        double y = Double.parseDouble(b);
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
