4b92cc83bab419b7907fd75ce58786deb0772420
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-09-22 14:28:14 +0800

    Operator Aritmatika Penjumlahan (+) dan Pengurangan (-)

---
+++ Day15.java
@@ -0,0 +1,15 @@
+public class Day15 {
+        public static void main(String[] args) {
+            int a = 20;
+            int b = 10;
+
+            int tambah = a + b;
+            int kurang = b - a;
+
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+            System.out.println("Hasil Penjumlahan (a + b) = " + tambah);
+            System.out.println("Hasil Pengurangan (a - b) = " + kurang);
+    }
+
+}
