ed946837f7799513c10b2ddeaafc24822588a93e
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-09-23 16:37:19 +0800

    Operator Aritmatika Perkalian (*) dan Pembagian (/)

---
+++ Day16.java
@@ -0,0 +1,14 @@
+public class Day16 {
+    public static void main(String[] args) {
+        int x = 50;
+        int y = 5;
+
+        int kali = x * y;
+        int bagi = x / y;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("Hasil Perkalian (x * y) = " + kali);
+        System.out.println("Hasil Pembagian (x / y) = " + bagi);
+    }
+}
