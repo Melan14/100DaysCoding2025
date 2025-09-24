dc9985697c12839a7fb7a2fab307844a1e3c0dac
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-09-24 23:21:44 +0800

    operator aritmatika modulus atau sisa bagi (%)

---
+++ Day17.java
@@ -0,0 +1,13 @@
+public class Day17 {
+        public static void main(String[] args) {
+            int angka1 = 25;
+            int angka2 = 4;
+
+            int hasil = angka1 % angka2;
+
+            System.out.println("Angka 1 : " + angka1);
+            System.out.println("Angka 2 : " + angka2);
+            System.out.println("Hasil " + angka1 + " % " + angka2 + " = " + hasil);
+    }
+
+}
