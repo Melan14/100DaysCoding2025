93ec4795a6bcb7d90b7c418686addc8079f9c130
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-09-26 20:39:13 +0800

    Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).

---
+++ Day19.java
@@ -0,0 +1,9 @@
+public class Day19 {
+    public static void main(String[] args) {
+        int angkaBesar = 110;
+        byte angkaKecil = (byte) angkaBesar;
+
+        System.out.println("Angka int   : " + angkaBesar);
+        System.out.println("Angka byte  : " + angkaKecil);
+    }
+}
