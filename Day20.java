191ef557f20ae086786395d2b34858938058d2ca
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-09-27 21:24:29 +0800

    Mengubah tipe data primitif menjadi string

---
+++ Day20.java
@@ -0,0 +1,11 @@
+public class Day20 {
+    public static void main(String[] args) {
+        int angka = 100;
+        String string = String.valueOf(angka);
+
+        // Cetak hasil
+        System.out.println("Angka int   : " + angka);
+        System.out.println("Angka String: " + string);
+    }
+
+}
