597023bda229beded6b3f38d529bfabb44d3da5c
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-09-29 21:22:30 +0800

    Latihan : Program Luas Persegi

---
+++ Day22.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Day22 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang sisi persegi: ");
+        int sisi = input.nextInt();
+
+        // Hitung luas
+        int luas = sisi * sisi;
+        System.out.println("Luas persegi dengan sisi " + sisi + " adalah: " + luas);
+    }
+}
