3b80abe63e6a3876574c69760f24e4424b0bca4d
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-10-02 19:24:55 +0800

    Operator Increment dan Decrement (++, --)

---
+++ Day25.java
@@ -0,0 +1,12 @@
+public class Day25 {
+    public static void main(String[] args) {
+        int angka = 10;
+
+        System.out.println("Nilai awal : " + angka);
+
+        angka++; // increment
+        System.out.println("Setelah ++ : " + angka);
+        angka--; // decrement
+        System.out.println("Setelah -- : " + angka);
+    }
+}
