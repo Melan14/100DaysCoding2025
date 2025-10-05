cd6e3c2dbb9129f90fb6f9204524f9b3b667a339
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-10-05 17:22:16 +0800

    Operator Perbandingan == dan !=

---
+++ Day28.java
@@ -0,0 +1,13 @@
+public class Day28 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = 2;
+
+        System.out.println("Apakah a == b? " + (a == b));
+        System.out.println("Apakah a == c? " + (a == c));
+
+        System.out.println("Apakah a != b? " + (a != b));
+        System.out.println("Apakah a != c? " + (a != c));
+    }
+}
