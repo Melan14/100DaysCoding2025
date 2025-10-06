5952e3733f4eb6e7d1f1f32638471debd2165b6b
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-10-06 21:49:10 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,12 @@
+public class OperatorPerbandingan {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+      
+        boolean LebihKecil = a < b;  
+        System.out.println(a + " < " + b + " = " + LebihKecil);
+
+        boolean LebihBesar = a > b;   
+        System.out.println(a + " > " + b + " = " + LebihBesar);
+    }
+}
