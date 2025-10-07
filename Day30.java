d1b762659c0c91905f4552ff33df72c391b245f7
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-10-07 23:47:24 +0800

    Operator Perbandingan <= dan >=

---
+++ Day30.java
@@ -0,0 +1,15 @@
+public class Day30 {
+        public static void main(String[] args) {
+            int angka1 = 10;
+            int angka2 = 15;
+
+            // Operator >= (lebih besar sama dengan)
+            boolean BesarSama = angka1 >= angka2;
+            System.out.println(angka1 + " >= " + angka2 + " = " + BesarSama);
+
+            // Operator <= (lebih kecil sama dengan)
+            boolean KecilSama = angka1 <= angka2;
+            System.out.println(angka1 + " <= " + angka2 + " = " + KecilSama);
+
+    }
+}
