48ed1143afa3d2cbe6b6f059c1fe48100372ca21
Melan14 <147216530+Melan14@users.noreply.github.com>
2025-10-29 22:51:53 +0800

    Create Day52.java

---
+++ Day52.java
@@ -0,0 +1,11 @@
+public class Day52 {
+    public static void main(String[] args) {
+        int i = 1; 
+
+        while (i <= 5) {
+            System.out.println("Perulangan ke-" + i);
+            i++; 
+        }
+        System.out.println("Perulangan selesai!");
+    }
+}
